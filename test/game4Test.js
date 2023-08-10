defb9105bec12de1e2840ab95d3b0a678be98598
ZeroHedgeK <83179771+ZeroHedgeK@users.noreply.github.com>
2023-08-10 12:51:19 +0300

    Contract puzzles all solved!

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,19 +1,25 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const signer = ethers.provider.getSigner(2);
+    const signerY = ethers.provider.getSigner(1);
+    const signerAddress = signer.getAddress();
+    const addressY = signerY.getAddress();
+
+    return { game, signer, signerY, signerAddress, addressY};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer, signerY, signerAddress, addressY } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
-
-    await game.win();
+    await game.connect(signerY).write(signerAddress);
+    await game.connect(signer).win(addressY);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
